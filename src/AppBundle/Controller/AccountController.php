870d8dcd154446a4288ee0d68e0765774da5647b
Mohammed Ali <mohammed_ali.aziz_assi@ldc.lu.se>
2021-02-01 14:36:01 +0100

    Added login handler

--- src/AppBundle/Controller/AccountController.php
+++ src/AppBundle/Controller/AccountController.php
@@ -2,12 +2,14 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Form\LoginFormType;
 use AppBundle\Form\RegistrationFormHandler;
 use AppBundle\Form\RegistrationFormType;
 use CustomerManagementFrameworkBundle\CustomerProvider\CustomerProviderInterface;
 use CustomerManagementFrameworkBundle\CustomerSaveValidator\Exception\DuplicateCustomerException;
 use CustomerManagementFrameworkBundle\Model\CustomerInterface;
 use CustomerManagementFrameworkBundle\Security\Authentication\LoginManagerInterface;
+use Pimcore\Bundle\EcommerceFrameworkBundle\Factory;
 use Pimcore\Translation\Translator;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -15,7 +17,9 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 /**
  * Class AccountController
@@ -24,6 +28,48 @@ use Symfony\Component\Security\Core\User\UserInterface;
  */
 class AccountController extends BaseController
 {
+    /**
+     * @Route("/account/login", name="account-login")
+     *
+     * @param AuthenticationUtils $authenticationUtils
+     * @param SessionInterface $session
+     * @param Request $request
+     * @param UserInterface|null $user
+     *
+     * @return array|RedirectResponse
+     */
+    public function loginAction(
+        AuthenticationUtils $authenticationUtils,
+        SessionInterface $session,
+        Request $request,
+        UserInterface $user = null
+    ) {
+
+        // Redirect user to index page if logged in
+        if ($user && $this->isGranted('ROLE_USER')) {
+            return $this->redirectToRoute('account-index');
+        }
+
+        // Get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // Last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        $formData = [
+            '_username' => $lastUsername
+        ];
+
+        $form = $this->createForm(LoginFormType::class, $formData, [
+            'action' => $this->generateUrl('account-login'),
+        ]);
+
+        return [
+            'form' => $form->createView(),
+            'error' => $error
+        ];
+    }
+
     /**
      *
      * @Route("/account/register", name="account-register")
@@ -116,8 +162,19 @@ class AccountController extends BaseController
             'customer' => $customer,
             'form' => $form->createView(),
             'errors' => $errors,
-            'hideBreadcrumbs' => true,
             'hidePassword' => $hidePassword
         ];
+    }
+
+    /**
+     * Index page for account
+     *
+     * @Route("/account/index", name="account-index")
+     */
+    public function indexAction()
+    {
+        return new Response(
+            '<h1>index page</h1>'
+        );
     }
 }
