f4652f4b380942b3f0d2c51ee4a4c53283b51d27
Mohammed Ali <mohammed_ali.aziz_assi@ldc.lu.se>
2021-02-16 21:03:07 +0100

    Added tools document-type

--- var/config/document-types.php
+++ var/config/document-types.php
@@ -26,5 +26,18 @@ return [
         "priority" => 0,
         "creationDate" => 1612266853,
         "modificationDate" => 1612266878
+    ],
+    3 => [
+        "id" => 3,
+        "name" => "Tools start page",
+        "group" => NULL,
+        "module" => "AppBundle",
+        "controller" => "@AppBundle\\Controller\\ToolsController",
+        "action" => "start",
+        "template" => NULL,
+        "type" => "page",
+        "priority" => 0,
+        "creationDate" => 1613502323,
+        "modificationDate" => 1613502359
     ]
 ];
