2b08ebf175ce506b045b8fc88d7c7efeff66be89
Mohammed Ali <mohammed_ali.aziz_assi@ldc.lu.se>
2021-02-17 15:38:30 +0100

    Added error-page

--- src/AppBundle/Controller/ContentController.php
+++ src/AppBundle/Controller/ContentController.php
@@ -5,12 +5,10 @@ namespace AppBundle\Controller;
 use Pimcore\Controller\FrontendController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class ContentController extends BaseController
 {
     /**
-     * @Template
      * @param Request $request
      */
     public function defaultAction(Request $request)
