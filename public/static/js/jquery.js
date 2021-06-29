809899080fc25e4581181ce8be160c0612896e2e
Mohammed Ali <mohammed_ali.aziz_assi@ldc.lu.se>
2021-06-29 11:24:38 +0200

    Added css + js

--- public/static/js/jquery.js
+++ public/static/js/jquery.js
@@ -5,4 +5,7 @@ $(function() {
         $('#navbarTogglerIconOpen').toggle();
         $('#navbarTogglerIconClose').toggle();
     })
+
+    // Add class to YouTube iframe element
+    $("iframe[src^='https://www.youtube']").addClass("rounded-3");
 });
